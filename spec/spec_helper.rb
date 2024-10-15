bd762f550a20d8f560b2fecf35f5ce8edd678dc3
DariaKalinina25 <meuw.meuw20@gmail.com>
2024-10-16 00:09:11 +0500

    Add formatted output for errors and random test execution in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
+  # Output error information in documentation format
+  config.formatter = :documentation
+
+  # Adds details about the method call chain in matchers
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  # Checks that methods called on the mock actually exist
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
 
+  # Automatically applies metadata from shared_context to all related groups and tests
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  # Runs tests in random order to identify dependent tests
+  config.order = :random
+  Kernel.srand config.seed
 end
