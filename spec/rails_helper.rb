065709a81688d7eeacfed5b5944b4eed4e416e10
DariaKalinina25 <meuw.meuw20@gmail.com>
2024-10-17 22:43:34 +0500

    Add Capybara and FactoryBot to rails_helper.rb and remove fixtures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara/rails'
+require 'factory_bot_rails'
 
+# Checks that all migrations are applied in the test database
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -13,13 +17,15 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 RSpec.configure do |config|
-  config.fixture_paths = [
-    Rails.root.join('spec/fixtures')
-  ]
+  # Includes the create and build methods without the FactoryBot prefix
+  config.include FactoryBot::Syntax::Methods
 
+  # Enables transactions to automatically roll back changes after each test
   config.use_transactional_fixtures = true
 
+  # Defines the test type
   config.infer_spec_type_from_file_location!
-
+  
+  # Hides internal Rails methods in errors for easier reading
   config.filter_rails_from_backtrace!
 end
