9a581b2c1a4797cc931a700c28d609f4e8824f95
DariaKalinina25 <meuw.meuw20@gmail.com>
2024-10-19 20:20:55 +0500

    Change factory User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,8 @@
 
 FactoryBot.define do
   factory :user do
-    name { 'John Doe' }
-    email { 'john.doe@example.com' }
+    email { 'test@example.com' }
+    password { 'password123' }
+    password_confirmation { 'password123' }
   end
 end
