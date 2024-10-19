2d2278d0618f36970750c74f8705417469950e16
DariaKalinina25 <meuw.meuw20@gmail.com>
2024-10-19 21:22:09 +0500

    Create test for model User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { build(:user) }
+
+    # Validation of email presence and uniqueness
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+
+    # Validation for correct email
+    it { is_expected.to allow_value('user@example.com').for(:email) }
+
+    # Validation for invalid email
+    invalid_emails = ['invalid_email', 'user@.com', '@domain.com', 'user@domain..com']
+
+    invalid_emails.each do |invalid_email|
+      it { is_expected.not_to allow_value(invalid_email).for(:email) }
+    end
+
+    # Validation for presence, uniqueness and length
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_confirmation_of(:password) }
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+  end
 end
