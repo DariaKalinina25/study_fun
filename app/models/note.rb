d196ecbed67714855e3ab6c54bba1f6f665f32d5
DariaKalinina25 <meuw.meuw20@gmail.com>
2024-11-12 22:55:21 +0500

    Add associations to the User and Note models

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Note < ApplicationRecord
+  belongs_to :user
+  belongs_to :folder
+
+  validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@
 
 # User model with Devise authentication modules.
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :folders, dependent: :destroy
+  has_many :notes, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
